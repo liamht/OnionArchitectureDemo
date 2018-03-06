4bcdd0a80e7116917883c7278fc94971e7d91092
Liam Hayman Tansley <liam.haymantansley@openprojects.co.uk>
2018-03-06 10:45:10 +0000

    Added first xUnit test for data.static layer
    Required changing all projects to netcoreapp instead of net standard - which was fine

---
+++ OnionArchitectureDemo.Data.Static.Tests/StaticDataUnitOfWorkTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Moq;
+using OnionArchitectureDemo.Data.Static.Cars;
+using OnionArchitectureDemo.Data.Static.Common;
+using OnionArchitectureDemo.Data.Static.Sales;
+using OnionArchitectureDemo.Domain.Entities;
+using Xunit;
+
+namespace OnionArchitectureDemo.Data.Static.Tests
+{
+    public class StaticDataUnitOfWorkTests
+    {
+        private StaticDataUnitOfWork _subject;
+
+        public StaticDataUnitOfWorkTests()
+        {
+            _subject = new StaticDataUnitOfWork();
+        }
+
+        [Fact]
+        public void Cars_ReturnsCarRepository()
+        {
+            var carType = _subject.Cars.GetType();
+            var repositoryType = typeof(CarRepository);
+
+            Assert.Equal(carType, repositoryType);
+        }
+
+
+        [Fact]
+        public void Sales_ReturnsSaleRepository()
+        {
+            var carType = _subject.Sales.GetType();
+            var repositoryType = typeof(SalesRepository);
+
+            Assert.Equal(carType, repositoryType);
+        }
+    }
+}
