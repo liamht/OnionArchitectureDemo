cde61f3e54ceec27f31d5cdac9581530af8c52b7
Liam Hayman Tansley <liamhaymantansley@easywebstore.net>
2019-03-05 15:00:51 +0000

    removed old project

--- OnionArchitectureDemo/Logic/Cars/Services/ICreateCarViewModelFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using OnionArchitectureDemo.Web.Logic.Cars.ViewModels;
-
-namespace OnionArchitectureDemo.Web.Logic.Cars.Services
-{
-    public interface ICreateCarViewModelFactory
-    {
-        CreateCarViewModel Create();
-    }
-}
